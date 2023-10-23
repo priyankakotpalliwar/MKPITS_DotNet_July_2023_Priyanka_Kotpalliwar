63a9a66d74a455a822fc6d96e87e05167b8d65e2
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-10-23 22:17:04 +0530

    files

---
+++ DOT NET/C_SHARP/assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int outercounter = 1; outercounter <= 3; outercounter++)
+            {
+                for (int innercounter = 1; innercounter <= outercounter; innercounter++)
+                {
+                    Console.Write(innercounter + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ DOT NET/C_SHARP/assignment2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int counter = 1;
+            Console.WriteLine("enter number");//3
+            number = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            while (true)
+            {
+                if (sum <= number)
+                {
+                    if (counter % 2 != 0)
+                    {
+                        Console.WriteLine("odd no : {0}", counter);
+                        sum = sum + 1;
+
+                    }
+
+                }
+                else
+                {
+                    break;
+                }
+                counter++;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
