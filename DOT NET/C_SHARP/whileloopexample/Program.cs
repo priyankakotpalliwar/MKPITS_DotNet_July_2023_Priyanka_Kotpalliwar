a3c059dd7f65f7a64b28d5e4f631b5c4dfa0e9e9
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-10-24 22:44:11 +0530

    files

---
+++ DOT NET/C_SHARP/whileloopexample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileloopexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n <= 10) {
+                
+                n++;
+            }
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
