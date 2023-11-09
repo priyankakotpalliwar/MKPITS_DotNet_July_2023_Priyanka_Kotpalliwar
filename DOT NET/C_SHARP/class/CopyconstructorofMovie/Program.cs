858f1561ab36fc8981badb30a759a9b48e483a14
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-11-09 22:42:16 +0530

    files

---
+++ DOT NET/C_SHARP/class/CopyconstructorofMovie/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyconstructorofMovie
+{
+    class movie
+    {
+        int time;
+        string moviename;
+
+
+
+        public movie() {
+
+
+            time = 7;
+            moviename = "heropanti";
+        }
+        public movie(movie m1)
+        {
+            time= m1.time;
+            moviename = "pagalpanti";
+        }
+        public void display()
+        {
+            Console.WriteLine(" time " + time);
+            Console.WriteLine("moviename "+moviename);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            movie mov= new movie();
+            movie mov2= new movie();
+
+            Console.WriteLine("movie theater 1 ");
+            mov.display();
+            Console.WriteLine("movie theater 2 ");
+            mov2 .display();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DOT NET/C_SHARP/class/copyconstructor/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace copyconstructor
+{
+    class book
+    {
+        int bookid;
+        string title;
+
+        public book()
+
+        {
+            bookid =1;
+            title = "vb.net";
+
+
+
+        }
+        public book(book b2)
+        {
+            bookid = b2.bookid;
+            title = b2.title;
+
+
+        }
+        public void display()
+        {
+            Console.WriteLine("book id of the book is "+ bookid);
+            Console.WriteLine("title of  the book id "+ title);
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book();
+            book book2 = new book();
+
+            Console.WriteLine("book 1 details    ");
+           book1.display();
+            Console.WriteLine(" book 2 details  ");
+            book2.display();
+              
+
+            Console.ReadKey();
+           
+        }
+    }
+}
