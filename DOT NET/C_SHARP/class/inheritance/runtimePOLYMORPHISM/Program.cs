ddb8fc356afcab3c92e0dfa4402c0048ea76b4f8
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-11-19 23:49:39 +0530

    files

---
+++ DOT NET/C_SHARP/class/inheritance/runtimePOLYMORPHISM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runtimePOLYMORPHISM
+{
+   class baseclass
+    {
+        public virtual  void method()
+        {
+            Console.WriteLine("hello from public virtual void");
+
+        }
+    }
+    class childclass:baseclass
+    {public override void method()
+        {
+            Console.WriteLine("hello from overide  void method   ");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            baseclass b1= new childclass();
+            b1.method();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DOT NET/C_SHARP/class/inheritance/runtimepolymorhism2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runtimepolymorhism2
+{
+    class animal
+    {
+        public virtual  void show()
+        {
+            Console.WriteLine("helooo");
+        }
+    }
+    class dog:animal
+    {
+        public override void show()
+        {
+            Console.WriteLine("dog bark");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            animal a1=new dog();
+            a1.show();
+            Console.ReadKey();
+        }
+    }
+}
