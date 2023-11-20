cc9a6fc9ea59486db9fc0cac14fafc966e845980
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-11-20 23:01:02 +0530

    files

---
+++ DOT NET/C_SHARP/abstraction/abstract class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract_class
+{
+    abstract class shape
+    {
+        public abstract void draw();
+    }
+    class rectangle : shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("code to draw rectangle");
+        }
+    } 
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            shape s = new rectangle();
+            s.draw();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DOT NET/C_SHARP/class/inheritance/runtimepolymorphismwithderivedCLASS/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runtimepolymorphismwithderivedCLASS
+{
+    class drawing
+    {
+        public virtual void draw() {
+
+            Console.WriteLine("drawing   ");
+
+        }
+    }
+    class rectangle:drawing
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drwaing rectangle");
+        }
+    }
+
+    class circle:drawing
+    {
+        public override void draw()
+        {
+            Console.WriteLine("drawing circle");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            drawing d1;
+            d1=new drawing();
+            d1.draw();
+            d1= new rectangle()
+                ;
+            d1.draw() ;
+            d1= new circle()
+                ; d1.draw() ;
+            Console.ReadKey();
+
+        }
+    }
+}
