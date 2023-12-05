5c4a4945dde21327bcadb16a58f8508d3296240e
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-12-05 11:55:00 +0530

    files

---
+++ DOT NET/WINFORM/2darray/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2darray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] num= new string [1,2];
+        StringBuilder sb=new StringBuilder();
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            num[0, 0] = textBox1.Text;
+            num[0,1]=textBox2.Text;
+            sb.Append("album name - " + num[0, 0] + "\nsinger name - " + num[0, 1]);
+            richTextBox1.Text = sb.ToString();
+
+        }
+    }
+}
---
+++ DOT NET/WINFORM/creating2darrayfor2rowsand2coloumbs/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace creating2darrayfor2rowsand2coloumbs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] details = new string[2, 2];
+        StringBuilder sb = new StringBuilder();
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            details[0,0]=textBox1.Text;
+            details[0,1]=textBox2.Text;
+            details[1,0]=textBox3.Text;
+            details[1,1]=textBox4.Text;
+            sb.Append("movie name - " + details[0,0]+"\nsong - " + details[0,1]);
+            sb.Append("\nactor - " + details[1, 0] + "\nactoress - " + details[1, 1]);
+            richTextBox1.Text = sb.ToString();
+
+        }
+    }
+}
