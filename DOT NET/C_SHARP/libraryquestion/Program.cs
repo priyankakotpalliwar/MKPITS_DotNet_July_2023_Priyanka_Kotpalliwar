4f0c53a33cb965a0a56beb718f8e3a7e6b02a8ea
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2023-12-11 23:12:22 +0530

    files

---
+++ DOT NET/C_SHARP/libraryquestion/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libraryquestion
+{
+    //: Implement a simple library management system in C#.
+    //Design classes for Book, Author, and Library.
+    //Each book should have properties like Title, ISBN, and a reference to an Author.
+    //The Library class should keep track of a collection of books.
+    //Implement methods for adding a book, removing a book, and displaying
+    //the list of books with their details. Ensure that the system handles the scenario
+    //where multiple books share the same title but have different ISBNs.
+    //Write a program to demonstrate the functionality of your library management system.
+
+    class Author
+    {
+        string Authorname { get; set; }
+        public Author(string Authorname)
+        {
+            this.Authorname = Authorname;
+        }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string ISBM { get; set; }
+        public string reference { get; set; }
+
+        public Book(string Title, string ISBM, Author author)
+        {
+            this.Title = Title;
+            this.ISBM = ISBM;
+            this.reference = reference;
+        }
+        public override string ToString()
+        {
+            return "title" + Title + "ISBM" + ISBM + "refernce" + reference;
+        }
+    }
+    class library
+    {
+        List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+            Console.WriteLine("books added" + book);
+        }
+        public void RemoveBook(string ISBM)
+        {
+            Book bookToRemove = null;
+            foreach (var book in books)
+            {
+                if (book.ISBM == ISBM)
+                {
+                    bookToRemove = book;
+                    break;
+                }
+            }
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+                Console.WriteLine("books removed" + bookToRemove);
+            }
+            else
+            {
+                Console.WriteLine("books not found in library");
+            }
+        }
+
+            public void displaybook()
+            {
+                Console.WriteLine("book in library");
+
+                foreach (var book in books)
+                {
+                    Console.WriteLine(book);
+
+
+                }
+            }
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+           //create authors
+           Author author1=new Author("priyanka");
+            Author author2 = new Author("shreya");
+            // create books
+            Book book1 = new Book("subtile art ", "8921", author1);
+            Book book2 = new Book("inside out", "8925", author2);
+            Book book3 = new Book("rafta rafta", "3090", author1);
+            //create library
+            library lib = new library();
+
+             
+            //add books to library
+            lib.AddBook(book1);
+            lib.AddBook(book2);
+            lib.AddBook(book3);
+            //displaying book
+            lib.displaybook();
+            //remove book
+            lib.RemoveBook("32344");
+
+            //displaying
+            lib.displaybook();
+
+
+            Console.ReadKey();
+            }
+        }
+    }
+
---
+++ DOT NET/WINFORM/machinetestQuestioon/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace machinetestQuestioon
+{
+    class Animal
+    {
+        public string name;
+        public int age;
+        public Animal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public virtual void Makesound()
+        {
+            Console.WriteLine("thiss is sound");
+
+        }
+    }
+    class Mammal : Animal
+    {
+        public Mammal(string name, int age) : base(name, age)
+        {
+        }
+    }
+    class Reptile : Animal
+    {
+        public Reptile(string name, int age) : base(name, age)
+        {
+        }
+    }
+    class Bird : Animal
+    {
+        public Bird(string name, int age) : base(name, age)
+        {
+        }
+    }
+    class Lion : Mammal
+    {
+        public Lion(string name, int age) : base(name, age)
+        {
+
+        }
+        public override void Makesound()
+        {
+            Console.WriteLine("roarrr");
+        }
+    }
+    class Snake :Reptile
+        {
+        public Snake(string name, int age):base(name, age)
+        {
+            
+
+        }
+        public override void Makesound()
+        {
+            Console.WriteLine("hiss");
+        }
+
+    }
+    class Eagle:Bird
+    {
+        public Eagle(string name,int age):base(name, age)
+        {
+
+        }
+        public override void Makesound()
+        {
+            Console.WriteLine("crurr");
+        }
+    }
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Design a class hierarchy for a zoo simulation in C#.
+            //Consider having base classes for animals, such as Mammal, Reptile, and Bird.
+            //Implement specific derived classes for animals like Lion, Snake, and Eagle.
+            //Each animal should have common properties like Name, Age, and a method for making a sound.
+            //Additionally, implement methods or properties specific to each animal type.
+            //Create instances of these classes and demonstrate polymorphism by
+            //calling the common methods on objects of different derived classes.
+
+
+            Lion li = new Lion("simbha", 3);
+            Snake snake = new Snake("nagin", 9);
+            Eagle eagle = new Eagle("shumshera", 8);
+            Animal[]an= {li, snake,eagle};
+            foreach(var animal in an )
+            {
+                Console.WriteLine("name" + animal.name + "age " + animal.age);
+                animal.Makesound();
+
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
