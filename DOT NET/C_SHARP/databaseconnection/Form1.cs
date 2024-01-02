3a21f38f352d8580176c33a614d1b26e9fe7a7e2
priyankakotpalliwar <95484212+priyankakotpalliwar@users.noreply.github.com>
2024-01-02 13:16:41 +0530

    files

---
+++ DOT NET/C_SHARP/databaseconnection/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace databaseconnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // create connection with string
+            string str = "server=.\\SQLEXPRESS01;integrated security=true;database=Student_Connection";
+           
+            SqlConnection con  = null;
+            //use sql connection class to connect to sqlserver database
+            try
+            {
+                con = new SqlConnection(str);
+                con.Open();
+                //in connected env. you have to explicitly open the connection using open method
+                MessageBox.Show("connected succesfully");
+
+            }
+            catch(Exception ee) 
+            {
+                MessageBox.Show(ee.ToString());
+            }
+            finally 
+            { 
+                con.Close();
+            }
+
+        }
+    }
+}
---
+++ SQL/adoconnection.sql
@@ -0,0 +1,6 @@
+create table users(userid numeric(18,0) identity,username varchar (50),Password varchar(50))
+drop table users
+
+select * from users
+insert into users values('priyanka','ojefa');
+insert into users values('huzefa','huzefa18');
